a56db946d301ba1d640c120f8965737d10e54121
chugiyan-md <Chugiyanmyavuu@outlook.com>
2024-08-19 02:34:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,16 +9,16 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Sialkot, Lahore" //ur location
 
 //new
-global.botname = '𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur owner name
+global.botname = 'ᴄʜᴜɢɪyᴀɴ-ᴍᴅ' //ur bot name
+global.ownernumber = ['919778535328'] //ur owner number, dont add more than one
+global.ownername = 'ᴄʜᴜɢɪyᴀɴ' //ur owner name
 global.websitex = "https://youtube.com/GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
 global.wm = "Global Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD15' //script link
-global.packname = "Sticker By"
-global.author = "𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃\n\n+923444844060"
+global.packname = "₉₇ͨ₇ͪ₈ͧ₅ᷛ₃ͥ₅ⷱ₃ⷶ₂ᷠ₈"
+global.author = "₉₇ͨ₇ͪ₈ͧ₅ᷛ₃ͥ₅ⷱ₃ⷶ₂ᷠ₈"
 global.creator = "923444844060@s.whatsapp.net"
 global.xprefix = '.'
 global.premium = ["923444844060"] // Premium User
